b16f0a210a77d4c66df0f9cbeeefbd92543cbc30
김KimGyeongHa <rudgkrk11@naver.com>
2023-02-11 15:50:17 +0900

    jpa crud and jpql basic query

--- ex1-hello-jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,13 +16,50 @@ public class JpaMain {
         try {
             tx.begin();
 
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+            /* insert
 
-            em.persist(member);
+                Member member = new Member();
+                member.setId(1L);
+                member.setName("HelloA");
 
-            tx.commit();
+                em.persist(member);
+                tx.commit();
+
+            */
+           /* select
+
+                Member findMember = em.find(Member.class,1L );
+                System.out.println("id :" +  findMember.getId());
+                System.out.println("name : " + findMember.getName());
+
+            */
+
+            /* update
+
+                em.find(Member.class,1L).setName("HelloJPA");
+                tx.commit();
+
+            */
+
+            /* delete
+
+                em.remove(em.find(Member.class,1L));
+
+            */
+
+            /* JPQL (Entity 객체대상으로 query) <-> SQL(데이터베이스 테이블 대상으로 query)
+
+                List<Member> result = em.createQuery("select m from Member as m",Member.class)
+                        .setFirstResult(1)
+                        .setMaxResults(10)
+                        .getResultList();
+
+                for(Member member : result){
+                    System.out.println("name : " + member.getName());
+                }
+
+            */
+
         }catch (Exception e){
             tx.rollback();
         }finally {
